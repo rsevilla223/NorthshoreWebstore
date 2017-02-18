5da249324e8d746d6b025d418246e9ede2c68ae6
ryansevilla <rsevilla@luc.edu>
2017-02-17 18:01:18 -0600

    updated read and fixed some calls

--- Client/orderFunctions.js
+++ Client/orderFunctions.js
@@ -1,14 +1,14 @@
 var createOrder = function(order) {
-  postJSON("http://127.0.0.1:8081/orders/addOrder", order, function(err, data) {
+  postJSON("http://localhost:8081/orders/addOrder", order, function(err, data) {
     document.write("<br><br>Creating order of 3 Skittles...<br>");
   });
-  postJSON("http://127.0.0.1:8081/inventory/decrementInventory", order, function(err, data) {
+  postJSON("http://localhost:8081/inventory/decrementInventory", order, function(err, data) {
     document.write("Decrementing quantity from Inventory table...<br>");
   });
 }
 
 var showOrders = function(user) {
-  postJSON("http://127.0.0.1:8081/orders/getCustomerOrders", user, function(err, data) {
+  postJSON("http://localhost:8081/orders/getCustomerOrders", user, function(err, data) {
     for(var i = 0; i<data.length; i++) {
       document.write("<br>" + "Customer ID: " + data[i].customerId + "<br>" + "Order ID: " + data[i].orderId + "<br>" + "Product: "+ data[i].productId + "<br>" + "Quantity: " + data[i].quantity + "<br>");
     }
@@ -17,13 +17,13 @@ var showOrders = function(user) {
 }
 
 var updateOrder = function(order) {
-  postJSON("http://127.0.0.1:8081/orders/updateOrder", order, function(err, data) {
+  postJSON("http://localhost:8081/orders/updateOrder", order, function(err, data) {
     document.write("<br>Updating order...<br>");
   });
 }
 
 var deleteOrder = function(order) {
-  deleteJSON("http://127.0.0.1:8081/orders/deleteOrder", order, function(err, data) {
+  deleteJSON("http://localhost:8081/orders/deleteOrder", order, function(err, data) {
     document.write("<br>Deleting order...<br>");
   });
 }
--- Client/userFunctions.js
+++ Client/userFunctions.js
@@ -1,7 +1,7 @@
 //User Functions
 
 var getAllUsers = function() {
-  getJSON("http://127.0.0.1:8081/users/displayUsers", function(err, data) {
+  getJSON("http://localhost:8081/users/displayUsers", function(err, data) {
     console.log(data);
     document.write("Users: <br>");
     for(var i=0; i<data.length; i++){
@@ -11,20 +11,20 @@ var getAllUsers = function() {
 }
 
 var addUser = function(user) {
-  postJSON("http://127.0.0.1:8081/users/addUser", user, function(err, data) {
+  postJSON("http://localhost:8081/users/addUser", user, function(err, data) {
     document.write("Adding new user...");
     console.log(data);
   });
 }
 
 var updateUser = function(user) {
-  postJSON("http://127.0.0.1:8081/users/updateUser", user, function(err, data) {
+  postJSON("http://localhost:8081/users/updateUser", user, function(err, data) {
     console.log(data);
   });
 }
 
 var deleteUser = function(user) {
-  deleteJSON("http://127.0.0.1:8081/users/deleteUser", user, function(err, data) {
+  deleteJSON("http://localhost:8081/users/deleteUser", user, function(err, data) {
     console.log(data);
   });
 }
